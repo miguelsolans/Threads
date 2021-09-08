12f5899104f40494078207db9fb28d478329fa0d
Miguel Solans <dev.miguelsolans@gmail.com>
2021-09-08 22:09:48 +0100

    Added hasOlder and Unseen Counter Output

--- Threads/Models/Output/InboxOutput.swift
+++ Threads/Models/Output/InboxOutput.swift
@@ -9,8 +9,12 @@ import Foundation
 
 struct InboxOutput: Decodable {
     let threads: [ThreadOutput];
+    let hasOlder: Bool;
+    let unseenCount: Int
     
     enum CodingKeys: String, CodingKey {
         case threads = "threads"
+        case hasOlder = "has_older"
+        case unseenCount = "unseen_count"
     }
 }
